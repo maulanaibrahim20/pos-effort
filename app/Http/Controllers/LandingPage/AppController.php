02d2f5de4da1edba27cb4461b20094b203edfddc
Teguhriyadi <ilham.teguh55@gmail.com>
2024-04-20 01:23:35 +0700

    Add: Menambahkan Function Home

--- app/Http/Controllers/LandingPage/AppController.php
+++ app/Http/Controllers/LandingPage/AppController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers\LandingPage;
 
 use App\Http\Controllers\Controller;
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
+    public function home()
+    {
+        $data = [
+            "produk" => Produk::where("status", 1)->get()
+        ];
+
+        return view("landing-page.home", $data);
+    }
+
     public function landingPage()
     {
         return view("landing-page.main");
