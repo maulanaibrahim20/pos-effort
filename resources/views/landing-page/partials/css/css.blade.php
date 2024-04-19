35abc00a49b88d9a158272b164ccba65d60ffae9
Teguhriyadi <ilham.teguh55@gmail.com>
2024-04-19 09:44:57 +0700

    Add: Menambahkan View Landing Page

---
+++ resources/views/landing-page/partials/css/css.blade.php
@@ -0,0 +1,31 @@
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+
+<!-- bootstrap css -->
+<link id="rtl-link" rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/vendors/bootstrap.css">
+
+<!-- wow css -->
+<link rel="stylesheet" href="{{ url('landing-page') }}/assets/css/animate.min.css" />
+
+<!-- font-awesome css -->
+<link rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/vendors/font-awesome.css">
+
+<!-- feather icon css -->
+<link rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/vendors/feather-icon.css">
+
+<!-- slick css -->
+<link rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/vendors/slick/slick.css">
+<link rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/vendors/slick/slick-theme.css">
+
+<!-- Iconly css -->
+<link rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/bulk-style.css">
+<link rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/vendors/animate.css">
+
+<!-- Template css -->
+<link id="color-link" rel="stylesheet" type="text/css" href="{{ url('landing-page') }}/assets/css/style.css">
