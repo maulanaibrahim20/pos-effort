807be33f9b94413aedcb5dfbc1154eaccaa5e6f5
Teguhriyadi <ilham.teguh55@gmail.com>
2024-04-23 23:37:54 +0700

    Add: Menambahkan Routing

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::get("/landingPage", [AppController::class, "landingPage"]);
 Route::get("/keranjang/{id_produk}", [AppController::class, "keranjang"]);
 Route::get("/keranjang/{id_produk}/detail", [AppController::class, "detailProduk"]);
 Route::post("/keranjang/{id_produk}/detailQty", [AppController::class, "detailQty"]);
+Route::post("/checkout", [AppController::class, "checkout"]);
+
+Route::get("/transaksi/{no_invoice}", [AppController::class, "invoice"]);
 
 Route::middleware(['guest'])->group(function () {
 
