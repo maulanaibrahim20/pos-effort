58493bfa503094c87c0dc765eabc3c853429e25f
Teguhriyadi <ilham.teguh55@gmail.com>
2024-04-23 22:39:48 +0700

    Modified: Update Logic

--- resources/views/landing-page/partials/header.blade.php
+++ resources/views/landing-page/partials/header.blade.php
@@ -39,72 +39,74 @@
 </nav>
 
 @if (!empty(Auth::user()))
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">
-                        Keranjang Belanja
-                    </h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form action="" method="POST">
-                    @csrf
-                    <div class="modal-body">
-                        @forelse ($keranjangDetail as $item)
-                            <div class="card shadow">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-4 mb-3">
-                                            <img src="{{ URL::asset('produk_thumbnail/thumbnail_1.jpg') }}"
-                                                alt="Gambar Produk" class="img-fluid" style="height: 100%;">
-                                        </div>
-                                        <div class="col-md-8">
-                                            <span class="produk fw-bold">
-                                                {{ $item['produk']['nama'] }}
-                                            </span>
-                                            <br>
-                                            <small>
-                                                {{ $item['produk']['kategori'] }}
-                                            </small>
-                                            <p class="harga-produk">
-                                                Rp. {{ number_format($item['produk']['hargaProduk']) }}
-                                            </p>
-                                            <div class="form-group">
-                                                <div class="row align-items-center">
-                                                    <div class="col-md-4 mb-3">
-                                                        <input type="number" class="form-control" name="qty-produk"
-                                                            id="qty-produk" value="{{ $item['qty'] }}" min="1">
-                                                    </div>
-                                                    <div class="col-md-4 mb-3">
-                                                        <button class="btn btn-danger btn-xs" style="margin-right: 10px"
-                                                            id="qty-minus" onclick="qtyMinus($item['id'])">
-                                                            <i class="fa fa-minus"></i>
-                                                        </button>
-                                                        <button class="btn btn-primary btn-xs" id="qty-plus"
-                                                            onclick="qtyPlus($item['id'])">
-                                                            <i class="fa fa-plus"></i>
-                                                        </button>
+    @if (!empty($keranjangDetail))
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">
+                            Keranjang Belanja
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="modal-body">
+                            @forelse ($keranjangDetail as $item)
+                                <div class="card shadow mb-3">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-4 mb-3">
+                                                <img src="{{ URL::asset('produk_thumbnail/thumbnail_1.jpg') }}"
+                                                    alt="Gambar Produk" class="img-fluid" style="height: 100%;">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span class="produk fw-bold">
+                                                    {{ $item['produk']['nama'] }}
+                                                </span>
+                                                <br>
+                                                <small>
+                                                    {{ $item['produk']['kategori'] }}
+                                                </small>
+                                                <p class="harga-produk">
+                                                    Rp. {{ number_format($item['produk']['hargaProduk']) }}
+                                                </p>
+                                                <div class="form-group">
+                                                    <div class="row align-items-center">
+                                                        <div class="col-md-4 mb-3">
+                                                            <input type="number" class="form-control" name="qty-produk"
+                                                                id="qty-produk" value="{{ $item['qty'] }}" min="1">
+                                                        </div>
+                                                        <div class="col-md-4 mb-3">
+                                                            <button class="btn btn-danger btn-xs" style="margin-right: 10px"
+                                                                id="qty-minus" onclick="qtyMinus($item['id'])">
+                                                                <i class="fa fa-minus"></i>
+                                                            </button>
+                                                            <button class="btn btn-primary btn-xs" id="qty-plus"
+                                                                onclick="qtyPlus($item['id'])">
+                                                                <i class="fa fa-plus"></i>
+                                                            </button>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        @empty
-                        @endforelse
-                    </div>
-                    <div class="modal-footer">
-                        <button type="reset" class="btn btn-danger">
-                            <i class="fa fa-times"></i> Batal
-                        </button>
-                        <button type="submit" class="btn btn-primary">
-                            <i class="fa fa-book"></i> Buatkan Pesanan
-                        </button>
-                    </div>
-                </form>
+                            @empty
+                            @endforelse
+                        </div>
+                        <div class="modal-footer">
+                            <button type="reset" class="btn btn-danger">
+                                <i class="fa fa-times"></i> Batal
+                            </button>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-book"></i> Buatkan Pesanan
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+    @endif
 @endif
