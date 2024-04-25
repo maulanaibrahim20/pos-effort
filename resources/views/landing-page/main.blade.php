6090dd4203aee401a8a7348299987540a402c2d9
Teguhriyadi <ilham.teguh55@gmail.com>
2024-04-25 23:39:28 +0700

    Modified: Update Layout Main

--- resources/views/landing-page/main.blade.php
+++ resources/views/landing-page/main.blade.php
@@ -4,36 +4,49 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>
-        .: {{ config('app.name') }} - @yield("title") :.
+        .: {{ config('app.name') }} - @stack('title') :.
     </title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{ url('/css/bootstrap.min.css') }}" rel="stylesheet">
 
-        <style>
-            .nav-link {
-                color: white;
-                font-weight: bold;
-            }
+    <style>
+        .nav-link {
+            color: white;
+            font-weight: bold;
+        }
 
-            footer {
-                padding: 20px 0px;
-            }
-        </style>
+        footer {
+            padding: 20px 0px;
+        }
+    </style>
 </head>
 
 <body>
 
-    @include("landing-page.partials.header")
+    @include('landing-page.partials.header')
 
-    @stack("content-page")
+    @stack('content-page')
 
-    @include("landing-page.partials.footer")
+    @include('landing-page.partials.footer')
 
     <script src="{{ url('/js/bootstrap.min.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    @stack("javascript")
+    @if (session('success'))
+        <script type="text/javascript">
+            Swal.fire({
+                title: "Berhasil",
+                text: "{{ session('success') }}",
+                icon: "success"
+            });
+        </script>
+    @endif
+
+    @stack('javascript')
 </body>
 
 </html>
