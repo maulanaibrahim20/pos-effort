1e1eb18ae9e7e2752b5243ddaf4f67a791ef15b1
Teguhriyadi <ilham.teguh55@gmail.com>
2024-04-29 23:23:20 +0700

    Add: Menambahkan Favico

--- resources/views/landing-page/main.blade.php
+++ resources/views/landing-page/main.blade.php
@@ -8,6 +8,10 @@
     <title>
         .: {{ config('app.name') }} - @stack('title') :.
     </title>
+
+    <link rel="icon" href="{{ url('/assets') }}/images/brand/icon1.png" type="image/x-icon">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ url('/assets') }}/images/brand/icon1.png">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
